5ab28613eb4e429a63b8df1ed12aa718ace7d463
Junior_Trevisol <jr.trevis@hotmail.com>
2022-08-12 11:59:10 +0100

    aqui eh dev

--- category/models.py
+++ category/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class CategoryAPI(models.Model):
 
     CAT_OPT = [
-        ('Food', 'Food'),
-        ('Health', 'Health'),
-        ('Education', 'Education'),
-        ('Home', 'Home'),
-        ('Other', 'Other'),
+        ('F', 'Food'),
+        ('H', 'Health'),
+        ('E', 'Education'),
+        ('H', 'Home'),
+        ('O', 'Other'),
     ]
-    category_type = models.CharField(choices=CAT_OPT, max_length=20)
+    category_type = models.CharField(
+        choices=CAT_OPT, max_length=1, null=False, default='O')
--- expenses/models.py
+++ expenses/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from category.models import *
 
 
 class Expenses(models.Model):
+    CATEGORY_ID = CategoryAPI.CAT_OPT
     date = models.DateField(auto_now_add=True)
     description = models.CharField(max_length=255)
-    amount = models.DecimalField(max_digits=20, decimal_places=20)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    # category_name = models.ForeignKey(
+    #     CategoryAPI, related_name='expenses', on_delete=models.CASCADE, null=False, default='O')
+    category_opt = models.CharField(
+        choices=CATEGORY_ID, max_length=1, null=False, default='O')
